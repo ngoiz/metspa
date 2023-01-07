a57a070e7d747c348ba5eaf7db6cef0349fce871
Norberto Goizueta <ng213@ic.ac.uk>
2023-01-07 11:01:25 +0100

    Add columns as dataclasses

--- metspa/postprocess.py
+++ metspa/postprocess.py
@@ -1,6 +1,7 @@
 # Should replace processdata.py in the future
 
 import pandas as pd
+from dataclasses import dataclass
 
 
 class DailyDataProcess:
@@ -38,18 +39,50 @@ class DailyDataProcess:
 
     @classmethod
     def monthly_report(cls, df: pd.DataFrame) -> pd.DataFrame:
+        # TODO: create a dataclass for columns
+        # @dataclass
+        # Column:
+        #   column.short_name
+        #   column.long_name
+        #   column.aggfunc
+        #   column.parent_column
+        #  
+        # prec = Column("prec", "Total Precipitation [mm]", "sum", "prec")
         return (
             df.groupby(df.index.month)
             .agg(
                 prec=("prec", sum),
                 tmed=pd.NamedAgg(column="tmed", aggfunc="mean"),
+                tmin_med=pd.NamedAgg(column="tmin", aggfunc="mean"),
                 tmin_min=("tmin", min),
-                tmin_max=("tmin", max),
-                tmax_min=("tmax", min),
+                tmax_med=pd.NamedAgg(column="tmax", aggfunc="mean"),
                 tmax_max=("tmax", max),
                 velmedia_max=("velmedia", max),
-                racha_max=("racha", max),
             )
             .assign(prec_cumsum=lambda df_: df_.prec.cumsum())
             .sort_index(axis=1)
         )
+        
+@dataclass
+class Column:
+    short_name: str
+    long_name: str
+    aggfunc_name: str
+    parent_column: str
+    
+    @property
+    def aggfunc(self):
+        if self.parent_column and self.aggfunc_name:
+            return pd.NamedAgg(column=self.parent_column, aggfunc=self.aggfunc_name)
+        else:
+            return None
+    
+monthly_report_columns = [
+    Column("prec", "Total Precipitation [mm]", "sum", "prec"),
+    Column("tmed", "Average of average daily temperature [C]", "mean", "tmed"),
+    Column("tmin_med", "Average minimum daily temperature [C]", "mean", "tmin"),
+    Column("tmin_min", "Minimum daily temperature [C]", "min", "tmin"),
+    Column("tmax_med", "Average of daily maximum temperature [C]", "mean", "tmax"),
+    Column("tmax_max", "Maximum daily temperature [C]", "max", "tmax"),
+    Column("velmedia", "Median wind speed [kph]", "mean", "velmedia")
+]
